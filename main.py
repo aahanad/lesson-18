1dae5a44f10e54b0fb2e0f09d4743ad380516700
aahanad <150878761+aahanad@users.noreply.github.com>
2024-07-16 19:01:07 -0700

    Update main.py

--- main.py
+++ main.py
@@ -3,6 +3,8 @@ import random
 pygame.init()
 WIDTH=850
 HEIGHT=750
+flying=False
+game_over=False
 screen=pygame.display.set_mode((WIDTH,HEIGHT))
 pygame.display.set_caption("FLAPPY BIRD")
 bg=pygame.image.load("C:\Aahana\Game Dev 2\lesson8\Things\city.png")
@@ -23,14 +25,31 @@ class Flappy(pygame.sprite.Sprite):
         self.rect=self.image.get_rect()
         self.rect.center=x,y
         self.count=0
+        self.vel=0
+        self.click=False
     def update(self):
-        self.count=self.count+1
-        if self.count>5:
+        #gravity|
+        #       \/
+        if flying==True:
+            self.vel+=0.2
+            if self.vel >5:
+                self.vel=5
+
+            if self.rect.bottom <680: 
+                self.rect.y+=self.vel
+        # jump |
+        #      V
+        if game_over==False:
+            if pygame.mouse. get_pressed()[0]==1 and self.click ==False:
+                self.vel=-7
+                self.click=True
+            if pygame.mouse.get_pressed()[0]==0:
+                self.click=False   
             self.count=0
             self.index=self.index+1
             if self.index>=3:
                 self.index=0
-        self.image=self.images[self.index]
+            self.image=self.images[self.index]
 bird=Flappy(100,375)
 birdgroup=pygame.sprite.Group()
 birdgroup.add (bird) 
@@ -38,11 +57,19 @@ while True:
     for event in pygame.event.get():
         if event.type==pygame.QUIT:
             pygame.quit()
+        if event.type==pygame.MOUSEBUTTONDOWN and game_over==False and flying==False:
+            flying=True
     screen.blit(bg,(0,0))
     screen.blit(ground,(ground_x,680))
     birdgroup.draw(screen)
     birdgroup.update()
-    ground_x=ground_x-0.5
-    if abs(ground_x)>35:
-        ground_x=0
+    if bird.rect.bottom >680:
+        game_over=True
+        flying=False
+
+    if game_over==False:
+        ground_x=ground_x-0.5
+        if abs(ground_x)>35:
+            ground_x=0
+
     pygame.display.update()
